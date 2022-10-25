bd387d7ec4323a85b690ea887416701ada4f64db
Mykyta Rudenko <mykyta.rudenko04@gmail.com>
2022-10-26 00:50:49 +0300

    Добавьте файлы проекта.

---
+++ Database/Database.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.Data.SqlClient;
+
+namespace DatabaseHelper
+{
+    internal class Database
+    {
+        private string? connStr = null;
+        private SqlConnection? conn = null;
+        public Database(string str)
+        {
+            try
+            {
+                connStr = str;
+                conn = GetSqlConn();
+                Console.WriteLine("DB connection created successful!");
+                conn.Open();
+                Console.WriteLine("Connection opened!");
+            }
+            catch (SqlException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+        public SqlConnection GetSqlConn()
+        {
+            if (conn == null)
+            {
+                SqlConnection testConn = new SqlConnection(connStr);
+                return testConn;
+            }
+            else
+            {
+                Console.WriteLine("Connection already created!");
+                return conn;
+            }
+        }
+        public int ExecuteCommand(string command)
+        {
+            if (conn.State == System.Data.ConnectionState.Open)
+            {
+                return new SqlCommand(command, conn).ExecuteNonQuery();
+            }
+            else return 0;
+
+        }
+        public void Destroy()
+        {
+            if (conn.State == System.Data.ConnectionState.Open)
+            {
+                conn.Close();
+                Console.WriteLine("Connection closed!");
+            }
+            else Console.WriteLine("Connection did not open!");
+        }
+    }
+}
---
+++ Database/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace DatabaseHelper
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const string connStr = @"Data Source=DESKTOP-2DC9VDS\SQLEXPRESS;Initial Catalog=testdb;Integrated Security=True;TrustServerCertificate=True;";
+            Database db = new Database(connStr);
+            int i = db.ExecuteCommand("INSERT INTO test_table VALUES ('AAA', '13');");
+            Console.WriteLine("Inserted: " + i);
+            db.Destroy();
+        }
+    }
+}
